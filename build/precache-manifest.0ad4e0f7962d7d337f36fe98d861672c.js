5386bbe8fa830dd235f3f923ee97b2edf4296366
asgbjo <geiribj@gmail.com>
2020-05-15 10:40:09 +0000

    Add files via upload

---
+++ build/precache-manifest.0ad4e0f7962d7d337f36fe98d861672c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df7f8b710f59304771657becf5783f2",
+    "url": "/jurotjugotjugo/index.html"
+  },
+  {
+    "revision": "4671d6c5b9f730fe21ea",
+    "url": "/jurotjugotjugo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a6e77726fa027b63338",
+    "url": "/jurotjugotjugo/static/js/2.5dddcd16.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jurotjugotjugo/static/js/2.5dddcd16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4671d6c5b9f730fe21ea",
+    "url": "/jurotjugotjugo/static/js/main.192151e1.chunk.js"
+  },
+  {
+    "revision": "a291aa7777e53e332998",
+    "url": "/jurotjugotjugo/static/js/runtime-main.0736dfb1.js"
+  }
+]);
