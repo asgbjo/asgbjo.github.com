8d2122ec0919cb1f9385f611a2b5576c58bbb93d
Ásgeir Björnsson <geiribj@gmail.com>
2022-10-02 11:21:24 +0000

    site

---
+++ pages/index.js
@@ -0,0 +1,87 @@
+import Head from 'next/head'
+
+export default function Home() {
+  const name = 'Ásgeir Björnsson'
+
+  return (
+    <div className="container">
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <main>
+        <img className="profile" src="asgeirb.jpeg" alt={name} />
+        <h1 className="title">
+          {name}
+        </h1>
+        <p className="description">
+          Software developer in Iceland
+        </p>
+      </main>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background-color: LightSteelBlue;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 3rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        .profile {
+          border-radius: 50%;
+          width: 15rem;
+          margin-bottom: 1.5rem;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
